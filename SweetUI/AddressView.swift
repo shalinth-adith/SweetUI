029a0fbd0f456ea32387fabae9f195c9e85be533
shalinth-adith <shalinth.adith@outlook.com>
2025-07-14 12:50:53 +0530

    Updated UI

---
+++ SweetUI/AddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressView.swift
+//  SweetUI
+//
+//  Created by shalinth adithyan on 14/07/25.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView()
+}
