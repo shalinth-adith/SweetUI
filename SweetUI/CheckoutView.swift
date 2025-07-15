3af514010e8b58c19723a001bb50842e7ff2d393
shalinth-adith <shalinth.adith@outlook.com>
2025-07-15 13:05:21 +0530

    updated

---
+++ SweetUI/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  SweetUI
+//
+//  Created by shalinth adithyan on 15/07/25.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
