41d8a109b5bbf24170afa7aa9c2e51e7aaad1acb
shalinth-adith <shalinth.adith@outlook.com>
2025-07-17 13:19:56 +0530

    aded server to recieve order

--- SweetUI/CheckoutView.swift
+++ SweetUI/CheckoutView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct CheckoutView: View {
     var order : Order
+    
+    @State private var confirmationNumber = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView{
             VStack{
@@ -24,13 +28,43 @@ struct CheckoutView: View {
                 Text("Your total cost is \(order.cost, format:.currency(code: "INR"))")
                     .font(.title)
                 
-                Button("Place Order", action: {})
+                Button("Place Order"){
+                    task{
+                        await placeOrder()
+                    }
+                }
                     .padding(.vertical)
             }
         }
         .navigationTitle(Text("Checkout"))
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        .alert("thank you !",isPresented: $showingConfirmation){
+            Button("OK") { }
+        }message:{
+            Text(confirmationNumber)
+        }
+    }
+    func placeOrder() async{
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        do{
+            let (data, _) = try await URLSession.shared.upload(for:request,from:encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationNumber = "Order \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) placed successfully , will be on the way in 10-15 mins"
+            showingConfirmation = true
+        }catch {
+            print("checkout failed : \(error.localizedDescription)")
+        }
+        
     }
 }
 
--- SweetUI/Data.swift
+++ SweetUI/Data.swift
@@ -8,7 +8,18 @@
 import Foundation
 
 @Observable
-class Order{
+class Order: Codable{
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zipCode = "zip"
+    }
     static let types = ["Vanilla", "Chocolate", "Strawberry","Rainbow"]
     
     var type = 0
