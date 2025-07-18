12f7d577ebb9ca666e090c683a2c57b1b8e3461c
shalinth-adith <shalinth.adith@outlook.com>
2025-07-18 15:38:13 +0530

    added starting animated screen

---
+++ SweetUI/SprinkleDot.swift
@@ -0,0 +1,18 @@
+//
+//  SprinkleDot.swift
+//  SweetUI
+//
+//  Created by shalinth adithyan on 18/07/25.
+//
+
+import SwiftUI
+
+struct SprinkleDot: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SprinkleDot()
+}
---
+++ SweetUI/SprinkleStartView.swift
@@ -0,0 +1,18 @@
+//
+//  SprinkleStartView.swift
+//  SweetUI
+//
+//  Created by shalinth adithyan on 18/07/25.
+//
+
+import SwiftUI
+
+struct SprinkleStartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SprinkleStartView()
+}
