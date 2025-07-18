b92e98d2ab7eee8821cd49930b0acaf1b0645b74
shalinth-adith <shalinth.adith@outlook.com>
2025-07-18 14:51:53 +0530

    structure changed

---
+++ SweetUI/addcupcakeview.swift
@@ -0,0 +1,18 @@
+//
+//  addcupcakeview.swift
+//  SweetUI
+//
+//  Created by shalinth adithyan on 18/07/25.
+//
+
+import SwiftUI
+
+struct addcupcakeview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addcupcakeview()
+}
---
+++ SweetUI/cupcakeitem.swift
@@ -0,0 +1,8 @@
+//
+//  cupcakeitem.swift
+//  SweetUI
+//
+//  Created by shalinth adithyan on 18/07/25.
+//
+
+import Foundation
